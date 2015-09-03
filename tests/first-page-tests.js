4055f3ca157f1cd8b7e5ee9dd74ad821108e9cea
hris21 <handreev000@gmail.com>
2015-09-03 17:19:01 +0300

    Unit tests -

---
+++ tests/first-page-tests.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../client/view/js/mainGamepacgeCreation.js" />
+import 'mainGamepacgeCreation.js';
+//import {secondPageCreation} from '../client/view/js/mainGamepacgeCreation.js';
+
+describe('#design tests', function () {
+    it('expect #wrapper to be generated, which should be the first layer, serving asa a cointainer', function () {
+        var container = document.getElementsById('wrapper');
+        expect(container).to.exist;
+    });
+
+    //The children in the container are christmas tree like . /\  
+    
+    it('expect second layer to be generated with class .site-wrapper-inner', function () {
+        var firstChild = document.getElementsByClass('.site-wrapper-innter');
+        expect(firstChild).to.exist;
+    });
+    
+    it('expect second layer to be generated with class .cover-container', function () {
+        var secondChild = document.getElementByClass('.cover-container');
+        expect(secondChild).to.exist;
+    });
+    it('expect second layer to be generated with class .masthead clearfix', function () {
+        var headerNodes = document.getElementByClass('.masthead clearfix');
+        expect(headerNodes).to.exist;
+    });
+    it('expect second layer to be generated with class .inner cover', function () {
+        var coverNodes = document.getElementByClass('.inner cover');
+        expect(coverNodes).to.exist;
+    });
+    it('expect second layer to be generated with class .mastfoot', function () {
+        var footerNodes = document.getElementByClass('.mastfoot');
+        expect(footerNodes).to.exist;
+    });
+});
+
---
+++ tests/second-page-test.js
@@ -0,0 +1,67 @@
+﻿/// <reference path="../client/view/js/secondPageCreation.js" />
+import 'secondPageCreation.js';
+//import {secondPageCreation} from '../client/view/js/secondPageCreation.js';
+
+describe('#design tests', function () {
+    it('expect DOM to generate four rows', function () {
+        var categories = document.getElementsByClassName('row'),
+            totalRowsCount = 3;
+        expect(categories).to.have.length(totalCategoriesCount);
+    });
+});
+
+describe('#The head "Pick a category" sign tests', function () {
+    it('expect the first row to have one children - #header' , function(){
+        var firstRow = document.getElementsByClassName('row')[0],
+            children;
+        if(firstRow.hasChildNodes()) {
+            // Make sure that object is not empty and, that it has child nodes.
+            children = firstRow.childNodes;
+        }
+        expect(children).to.have.length(1);
+    });
+
+    it('expect the children to be the #header node' , function(){
+        var header = document.getElementById('header');
+        expect(header).to.exist;
+    });
+});
+
+describe('#Kids and School categories tests', function () {
+    it('expect the first to have #KidsAcademy and #SchoolAcademy nodes' , function(){
+        var secondRow = document.getElementsByClassName('row')[1],
+            children;
+        if(secondRow.hasChildNodes()) {
+            children = secondRow.childNodes;
+        }
+        expect(children).to.have.length(2);
+    });
+    it('expect #KidsAcademy to exist' , function(){
+        var kidsAcademy = document.getElementById('#KidsAcademy');
+        expect(kidsAcademy).to.exist;
+    });
+    it('expect #SchoolAcademy to exist' , function(){
+        var schoolAcademy  = document.getElementById('#SchoolAcademy');
+        expect(schoolAcademy).to.exist;
+    });
+});
+
+describe('#Software and Algo categories tests', function () {
+    it('expect the first to have #SoftwareAcademy and #AlgoAcademy nodes' , function(){
+        var thirdRow = document.getElementsByClassName('row')[2],
+            children;
+        if(thirdRow .hasChildNodes()) {
+            children = secondRow.childNodes;
+        }
+ 
+        expect(children).to.have.length(2);
+    });
+    it('expect #KidsAcademy to exist' , function(){
+        var softwareAcademy = document.getElementById('#SoftwareAcademy');
+        expect(softwareAcademy).to.exist;
+    });
+    it('expect #SchoolAcademy to exist' , function(){
+        var algoAcademy = document.getElementById('#AlgoAcademy');
+        expect(algoAcademy).to.exist;
+    });
+});
