65e75e6e6bec71f92b2b976792ec20e4fac994ce
hris21 <handreev000@gmail.com>
2015-09-04 02:58:00 +0300

    tests fixes

--- tests/first-page-tests.js
+++ tests/first-page-tests.js
@@ -1,34 +1,65 @@
-﻿/// <reference path="../client/view/js/mainGamepacgeCreation.js" />
-import 'mainGamepacgeCreation.js';
-//import {secondPageCreation} from '../client/view/js/mainGamepacgeCreation.js';
+﻿/// <reference path="../client/view/js/indexCreation.js" />
+
+import 'indexCreation.js';
+//import {indexCreation} from '../client/view/js/indexCreation.js';
 
 describe('#design tests', function () {
-    it('expect #wrapper to be generated, which should be the first layer, serving as a cоntainer', function () {
-        var container = document.body.getElementById('wrapper');
+    it('expect #wrapper to be generated, which should be the first layer, serving asa a cointainer', function () {
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var container = document.getElementById('wrapper');
         expect(container).to.exist;
     });
 
     //The children in the container are christmas tree like . /\  
     
     it('expect second layer to be generated with class .site-wrapper-inner', function () {
-        var firstChild = document.body.getElementByClass('.site-wrapper-innter');
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var firstChild = document.getElementByClass('.site-wrapper-innter');
         expect(firstChild).to.exist;
     });
     
     it('expect second layer to be generated with class .cover-container', function () {
-        var secondChild = document.body.getElementByClass('.cover-container');
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var secondChild = document.getElementByClass('.cover-container');
         expect(secondChild).to.exist;
     });
     it('expect second layer to be generated with class .masthead clearfix', function () {
-        var headerNodes = document.body.getElementByClass('.masthead clearfix');
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var headerNodes = document.getElementByClass('.masthead clearfix');
         expect(headerNodes).to.exist;
     });
     it('expect second layer to be generated with class .inner cover', function () {
-        var coverNodes = document.body.getElementByClass('.inner cover');
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var coverNodes = document.getElementByClass('.inner cover');
         expect(coverNodes).to.exist;
     });
     it('expect second layer to be generated with class .mastfoot', function () {
-        var footerNodes = document.body.getElementByClass('.mastfoot');
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/indexCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var footerNodes = document.getElementByClass('.mastfoot');
         expect(footerNodes).to.exist;
     });
 });
