32bcaa1c3006127b25a3e2c78aba3e96dae45928
hris21 <handreev000@gmail.com>
2015-09-04 02:51:05 +0300

    hotfix

--- tests/second-page-test.js
+++ tests/second-page-test.js
@@ -4,64 +4,109 @@ import 'secondPageCreation.js';
 
 describe('#design tests', function () {
     it('expect DOM to generate four rows', function () {
-        var categories = document.body.getElementsByClassName('row'),
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var categories = document.getElementsByClassName('row'),
             totalRowsCount = 3;
         expect(categories).to.have.length(totalCategoriesCount);
     });
 });
 
 describe('#The head "Pick a category" sign tests', function () {
-    it('expect the first row to have one children - #header', function () {
-        var firstRow = document.body.getElementsByClassName('row')[0],
+    it('expect the first row to have one children - #header' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var firstRow = document.getElementsByClassName('row')[0],
             children;
-        if (firstRow.hasChildNodes()) {
+        if(firstRow.hasChildNodes()) {
             // Make sure that object is not empty and, that it has child nodes.
             children = firstRow.childNodes;
         }
         expect(children).to.have.length(1);
     });
 
-    it('expect the children to be the #header node', function () {
-        var header = document.body.getElementById('header');
+    it('expect the children to be the #header node' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var header = document.getElementById('header');
         expect(header).to.exist;
     });
 });
 
 describe('#Kids and School categories tests', function () {
-    it('expect the first to have #KidsAcademy and #SchoolAcademy nodes', function () {
-        var secondRow = document.body.getElementsByClassName('row')[1],
+    it('expect the first to have #KidsAcademy and #SchoolAcademy nodes' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var secondRow = document.getElementsByClassName('row')[1],
             children;
-        if (secondRow.hasChildNodes()) {
+        if(secondRow.hasChildNodes()) {
             children = secondRow.childNodes;
         }
         expect(children).to.have.length(2);
     });
-    it('expect #KidsAcademy to exist', function () {
-        var kidsAcademy = document.body.getElementById('#KidsAcademy');
+    it('expect #KidsAcademy to exist' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var kidsAcademy = document.getElementById('#KidsAcademy');
         expect(kidsAcademy).to.exist;
     });
-    it('expect #SchoolAcademy to exist', function () {
-        var schoolAcademy = document.body.getElementById('#SchoolAcademy');
+    it('expect #SchoolAcademy to exist' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var schoolAcademy  = document.getElementById('#SchoolAcademy');
         expect(schoolAcademy).to.exist;
     });
 });
 
 describe('#Software and Algo categories tests', function () {
-    it('expect the first to have #SoftwareAcademy and #AlgoAcademy nodes', function () {
-        var thirdRow = document.body.getElementsByClassName('row')[2],
+    it('expect the first to have #SoftwareAcademy and #AlgoAcademy nodes' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var thirdRow = document.getElementsByClassName('row')[2],
             children;
-        if (thirdRow.hasChildNodes()) {
+        if(thirdRow .hasChildNodes()) {
             children = secondRow.childNodes;
         }
-
+ 
         expect(children).to.have.length(2);
     });
-    it('expect #SoftwareAcademy to exist', function () {
-        var softwareAcademy = document.body.getElementById('#SoftwareAcademy');
+    it('expect #SoftwareAcademyAcademy to exist' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var softwareAcademy = document.getElementById('#SoftwareAcademy');
         expect(softwareAcademy).to.exist;
     });
-    it('expect #SchoolAcademy to exist', function () {
-        var algoAcademy = document.body.getElementById('#AlgoAcademy');
+    it('expect #SchoolAcademy to exist' , function(){
+        var documentBody = document.body,
+            html = '<scripts>../client/view/js/secondPageCreation.js</scripts>';
+
+        documentBody.innerHTML += html;
+
+        var algoAcademy = document.getElementById('#AlgoAcademy');
         expect(algoAcademy).to.exist;
     });
 });
