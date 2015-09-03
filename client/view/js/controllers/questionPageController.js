212237e6241997e351cc5df74f6bf55e6f4adead
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-09-03 11:06:26 +0300

    Fix Sign out link oh third page to save on database

--- client/view/js/controllers/questionPageController.js
+++ client/view/js/controllers/questionPageController.js
@@ -4,7 +4,7 @@ import page3 from '../mainGamepageCreation.js';
 
 import implementator from '../events/implementingNextQuestion.js';
 import SignOut from '../../../model/signout.js';
-
+import scoreObj from '../../../model/score.js'
 function init() {
 
     // .js file for question page
@@ -23,11 +23,22 @@ function init() {
     implementator.questionChanger();
     $('#log-out-third-page').click(function(){
         alert('pred sign out');
-        SignOut.signOut().then(function(){
-            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa sign out");
-            window.location.assign("");
-        });
 
+        var username = Parse.User.current().get('username');
+        var score = localStorage.getItem('points');
+        var pointsObject = scoreObj.createGameScoreObject();
+        console.log(username);
+        console.log(score);
+        pointsObject.init(username, score);
+        pointsObject.save().then(function () {
+            SignOut.signOut();
+            localStorage.clear();
+            alert('The score is saved');
+            windows.location.assign('');
+
+        }, function (error) {
+            alert(error.message);
+        });
     });
 
 
