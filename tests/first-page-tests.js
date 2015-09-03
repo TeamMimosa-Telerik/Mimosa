c7062c476ba16aeab3ba853e12e52543e83e4427
hris21 <handreev000@gmail.com>
2015-09-03 22:20:43 +0300

    Fixing possible bugs.

--- tests/first-page-tests.js
+++ tests/first-page-tests.js
@@ -3,32 +3,32 @@ import 'mainGamepacgeCreation.js';
 //import {secondPageCreation} from '../client/view/js/mainGamepacgeCreation.js';
 
 describe('#design tests', function () {
-    it('expect #wrapper to be generated, which should be the first layer, serving asa a cointainer', function () {
-        var container = document.getElementsById('wrapper');
+    it('expect #wrapper to be generated, which should be the first layer, serving as a cоntainer', function () {
+        var container = document.body.getElementById('wrapper');
         expect(container).to.exist;
     });
 
     //The children in the container are christmas tree like . /\  
     
     it('expect second layer to be generated with class .site-wrapper-inner', function () {
-        var firstChild = document.getElementsByClass('.site-wrapper-innter');
+        var firstChild = document.body.getElementByClass('.site-wrapper-innter');
         expect(firstChild).to.exist;
     });
     
     it('expect second layer to be generated with class .cover-container', function () {
-        var secondChild = document.getElementByClass('.cover-container');
+        var secondChild = document.body.getElementByClass('.cover-container');
         expect(secondChild).to.exist;
     });
     it('expect second layer to be generated with class .masthead clearfix', function () {
-        var headerNodes = document.getElementByClass('.masthead clearfix');
+        var headerNodes = document.body.getElementByClass('.masthead clearfix');
         expect(headerNodes).to.exist;
     });
     it('expect second layer to be generated with class .inner cover', function () {
-        var coverNodes = document.getElementByClass('.inner cover');
+        var coverNodes = document.body.getElementByClass('.inner cover');
         expect(coverNodes).to.exist;
     });
     it('expect second layer to be generated with class .mastfoot', function () {
-        var footerNodes = document.getElementByClass('.mastfoot');
+        var footerNodes = document.body.getElementByClass('.mastfoot');
         expect(footerNodes).to.exist;
     });
 });
